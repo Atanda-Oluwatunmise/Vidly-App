712c3062bfb701a610a473d3d0dac940b6d026e7
Atanda Tunmise <atandatunmise@gmail.com>
2022-04-11 00:10:50 -0800

    completed the service that calls the endpoints to perform CRUD operations

--- index.js
+++ index.js
@@ -32,24 +32,59 @@ app.post('/vivdly.com/api/genres', (req, res) => {
 
     const genre = {
         id: genres.length + 1,
-        story: req.body.story
-    // genre.plot = req.body.plot;
-    // genre.character = req.body.character;
-    // genre.setting = req.body.setting;
+        story: req.body.story,
+        plot: req.body.plot,
+        character: req.body.character,
+        setting: req.body.setting
     };
     genres.push(genre);
     res.send(genre);
 })
 
+app.put('/vivdly.com/api/genres/:id', (req, res) => {
+    // lookup the genre
+    // if not existing, return 404-resource not found
+    const genre = genres.find(c => c.id === parseInt(req.params.id));
+    if (!genre) return res.status(404).send('The genre was not found');
+    
+
+    // Validate
+    // If invalid, return 400-Bad request
+    // object destructuring
+    const { error } = validateGenre(req.body) // equivalent to result.error
+    if (error) return res.status(400).send(error.details[0].message);
+
+    // Update genre
+    genre.story = req.body.story;
+    genre.plot = req.body.plot;
+    genre.character = req.body.character;
+    genre.setting = req.body.setting;
+
+    // Return updated genre
+    res.send(genre);
+});
+
+app.delete('/vivdly.com/api/genres/:id', (req, res) => {
+    const genre = genres.find(c => c.id === parseInt(req.params.id));
+    if (!genre) return res.status(404).send('The genre was not found');
+
+    // delete
+    const index  = genres.indexOf(genre);
+    genres.splice(index, 1);
+
+    // return the same course
+    res.send(genre);
+
+});
 
 function validateGenre(genre) {
-    const schema = {
-        story: Joi.string().min(2).required()
-        // plot: Joi.string().min(5).required(),
-        // character: Joi.string().min(3).required(),
-        // setting: Joi.string().min(5).required()
-    };
-    return Joi.validate(genre, schema);
+    const schema = Joi.object({
+        story: Joi.string().min(2).required(),
+        plot: Joi.string().min(5).required(),
+        character: Joi.string().min(3).required(),
+        setting: Joi.string().min(5).required()
+    });
+    return schema.validate(genre);
 
 }
 
